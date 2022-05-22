cd203ad790babfb442cb13a3926755cc49b85e2d
OwenLobato <owenlobatov@gmail.com>
2022-05-21 23:54:12 -0500

    'about_message_passing.rb' done

--- rubykoans/koans/about_proxy_object_project.rb
+++ rubykoans/koans/about_proxy_object_project.rb
@@ -13,12 +13,35 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_reader :messages
+
   def initialize(target_object)
     @object = target_object
     # ADD MORE CODE HERE
+    @messages = []
   end
 
   # WRITE CODE HERE
+  def called? method_name
+    @messages.include? method_name
+  end
+
+  def number_of_times_called method_name
+    times_called = 0
+    @messages.each do |message|
+      times_called += 1 if message == method_name
+    end
+    times_called
+  end
+
+  def method_missing method_name, *args, &block
+    if @object.respond_to? method_name
+      @messages.push method_name
+      @object.send method_name, *args
+    else
+      super method_name, *args, &block
+    end
+  end
 end
 
 # The proxy object should pass the following Koan:
